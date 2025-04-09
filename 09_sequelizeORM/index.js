ae6669541c81b0d40475cba430a466ebe73d642d
Vitor Scatine <vitor.scatine@ramper.com.br>
2025-04-08 23:06:28 -0300

    Sequelize - Fim

--- 09_sequelizeORM/index.js
+++ 09_sequelizeORM/index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const exphbs = require('express-handlebars')
-const conn = require('./db/conn')
+const express   = require('express')
+const exphbs    = require('express-handlebars')
+const conn      = require('./db/conn')
 
-const User = require('./models/User')
+const User      = require('./models/User')
+const Address   = require('./models/Address')
 
 const app = express()
 const port = 3000
@@ -29,7 +30,7 @@ app.get('/', async (req, res) => {
 })
 
 /* Insert*/
-app.post('/user/create', async (req, res) => {
+app.post('/users/create', async (req, res) => {
     const name = req.body.name
     const occupation = req.body.occupation
     let newsletter = req.body.newsletter
@@ -60,10 +61,81 @@ app.post('/users/delete/:id', async (req, res) => {
     res.redirect('/')
 })
 
-conn.sync()
-    .then(
-        app.listen(port, () => {
-            console.log(`App rodando na porta ${port}`)
-        })
-    )
-    .catch((err) => console.log(err))
+/* UPDATE */
+app.get('/users/edit/:id', async (req, res) => {
+    const idReq = req.params.id
+    const user = await User.findOne({
+        //Removendo raw = true para pegar endereços vinculados
+        //raw :   true, 
+        include: Address,
+        where : {
+            id: idReq
+        }
+    })
+    res.render('useredit', { user: user.get({plain : true}) })
+})
+
+app.post('/users/edit/:id', async (req, res) => {
+    const idReq     = req.body.id
+    const nameReq   = req.body.name
+    const occupReq  = req.body.occupation
+    let   newsReq   = req.body.newsletter
+    
+    !newsReq? newsReq = false : newsReq = true
+
+    await User.update({
+        name:       nameReq,
+        occupation: occupReq,
+        newsletter: newsReq
+    },
+    {
+        where: {
+            id: idReq
+        }
+    })
+
+    res.redirect('/')
+})
+
+app.post('/address/create/:id', async (req, res) => {
+    const userId = req.body.userId
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+
+    const adressData = {
+        userId,
+        street,
+        number,
+        city
+    }
+
+    await Address.create(adressData)
+
+    res.redirect(`/users/edit/${userId}`)
+})
+
+app.post('/address/delete/:id', async (req, res) => {
+    const addressId = req.body.id
+    const userId = req.body.userId
+
+    await Address.destroy({
+        where: {
+            id: addressId
+        }
+    })
+    
+
+    res.redirect(`/users/edit/${userId}`)
+})
+
+conn
+    //Drop na tabela e recria
+    //.sync({ force : true })
+    .sync()
+        .then(
+            app.listen(port, () => {
+                console.log(`App rodando na porta ${port}`)
+            })
+        )
+        .catch((err) => console.log(err))
---
+++ 09_sequelizeORM/models/jsjs.js
@@ -0,0 +1,51 @@
+user = {
+    dataValues: {
+      id: 3,
+      name: 'Dilmola',
+      occupation: 'Ex presidenta',
+      newsletter: true,
+      createdAt: 2025-04-09T01:12:59.000Z,
+      updatedAt: 2025-04-09T01:12:59.000Z,
+      adresses: [ [adress] ]
+    },
+    _previousDataValues: {
+      id: 3,
+      name: 'Dilmola',
+      occupation: 'Ex presidenta',
+      newsletter: true,
+      createdAt: 2025-04-09T01:12:59.000Z,
+      updatedAt: 2025-04-09T01:12:59.000Z,
+      adresses: [ [adress] ]
+    },
+    uniqno: 1,
+    _changed: Set(0) {},
+    _options: {
+      isNewRecord: false,
+      _schema: null,
+      _schemaDelimiter: '',
+      include: [ [Object] ],
+      includeNames: [ 'adresses' ],
+      includeMap: { adresses: [Object] },
+      includeValidated: true,
+      attributes: [
+        'id',
+        'name',
+        'occupation',
+        'newsletter',
+        'createdAt',
+        'updatedAt'
+      ],
+      raw: true
+    },
+    isNewRecord: false,
+    adresses: [
+      adress {
+        dataValues: [Object],
+        _previousDataValues: [Object],
+        uniqno: 1,
+        _changed: Set(0) {},
+        _options: [Object],
+        isNewRecord: false
+      }
+    ]
+  }
